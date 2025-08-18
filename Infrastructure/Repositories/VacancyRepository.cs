29ccce9ce7744a21f4af785615377f88ea731d0b
MahmoudAly <mahmoudaly964@gmail.com>
2025-08-18 17:27:42 +0300

    ✨ feat(infrastructure): implement VacancyRepository

---
+++ Infrastructure/Repositories/VacancyRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class VacancyRepository : Repository<Vacancy>, IVacancyRepository
+    {
+        public VacancyRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task PublishVacancy(Guid vacancyId)
+        {
+            var vacancy = await GetByIdAsync(vacancyId,tracking:true);
+            if (vacancy != null)
+            {
+                vacancy.IsPublished = true;
+                await UpdateAsync(vacancy);
+            }
+        }
+
+        public async Task UnPublishVacancy(Guid vacancyId)
+        {
+            var vacancy = await GetByIdAsync(vacancyId);
+            if (vacancy != null)
+            {
+                vacancy.IsPublished = false;
+                await UpdateAsync(vacancy);
+            }
+        }
+    }
+}
