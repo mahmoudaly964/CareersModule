e6b343e6131d1f88b8b71bf282179fada0650cc0
MahmoudAly <mahmoudaly964@gmail.com>
2025-08-18 17:01:44 +0300

    ✨ feat(infrastructure): implement the generic repository

--- Domain/Interfaces/IRepository.cs
+++ Domain/Interfaces/IRepository.cs
@@ -9,10 +9,10 @@ namespace Domain.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        Task<T?> GetByIdAsync(Guid id);
+        Task<T?> GetByIdAsync(Guid id,bool tracking=true);
         Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null,
                                         int? pageNumber = 1,
-                                        int? pageSize = 10);
+                                        int? pageSize = 10, bool tracking = true);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(Guid id);
---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Interfaces;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, bool tracking = true)
+        {
+            IQueryable<T> query = tracking ? _dbSet : _dbSet.AsNoTracking();
+
+            return await query.FirstOrDefaultAsync(e => EF.Property<Guid>(e, "Id") == id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null,
+                                             int? pageNumber = 1,
+                                             int? pageSize = 10,
+                                             bool tracking = true)
+        {
+            IQueryable<T> query = tracking ? _dbSet : _dbSet.AsNoTracking();  
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (pageNumber.HasValue && pageSize.HasValue)
+            {
+                query = query.Skip((pageNumber.Value - 1) * pageSize.Value)
+                           .Take(pageSize.Value);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+    }
+}
