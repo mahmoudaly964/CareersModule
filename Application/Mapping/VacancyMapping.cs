f0cd56d10d6a9e33b5c60ccd7119d0bf372c1119
MahmoudAly <mahmoudaly964@gmail.com>
2025-08-19 16:18:39 +0300

    ✨ feat(application): add Vacancy mapping

---
+++ Application/Mapping/VacancyMapping.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Vacancy;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class VacancyMapping : Profile
+    {
+        public VacancyMapping()
+        {
+            CreateMap<Vacancy, VacancyResponseDTO>().ReverseMap();
+            CreateMap<Vacancy, AddVacancyDTO>().ReverseMap();
+            CreateMap<Vacancy, UpdateVacancyDTO>().ReverseMap();
+        }
+    }
+}
